1aae49b8412c88cd550b52dd6b868369ac16fa8d
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-08-30 16:51:09 +0300

    Display reservation popup with selected item's details.

---
+++ src/modules/reservationPopup.js
@@ -0,0 +1,23 @@
+const foodContaier = document.querySelector('.card-container');
+
+export const revervationPopup= (totalFoodies) =>{
+  const reset=document.querySelectorAll(".reservation-btn");
+  reset.forEach((btn,index) =>{
+    btn.addEventListener("click", ()=>{
+      foodContaier.innerHTML='';
+      foodContaier.innerHTML += `
+          <div class="revervation-card">
+            <div class="img-x">
+              <img class="reserv-img" src="${totalFoodies[index][0].strMealThumb}" alt="${totalFoodies[index][0].strMeal}">
+              <span class="reser-button">X</span>
+            </div>
+            <h1 class="rever-description">${totalFoodies[index][0].strMeal}</h1>
+            <div class="resev-details">
+              <h3>${totalFoodies[index][0].strCategory}</h3>
+              <h3>${totalFoodies[index][0].strArea}</h3>
+            </div>
+        </div>
+      `;
+    });
+  })
+}
