d3b9d0f660ac589c93719e456718d08afbd3352d
tinayetsina <tinayetsina@outlook.com>
2022-09-03 00:38:39 +0800

    update - linter errors resolved

--- src/__mocks__/commentCounter.test.js
+++ src/__mocks__/commentCounter.test.js
@@ -1,7 +1,7 @@
 import { counter1 } from './commentCounter.js';
 
 test('Shows the number of comments', async () => {
-    const API_FETCH = [{ username: 'jane', comment: 'comment' }, { username: 'imer', comment: 'comment' }, { username: 'Cake', comment: 'comment' }, { username: 'Suzana', comment: 'comment' }, { username: 'Tinaye', comment: 'comment' }, { username: 'Andrea', comment: 'comment' }];
-    const length = await counter1(API_FETCH);
-    expect(length).toEqual(6);
-    });
+  const API_FETCH = [{ username: 'jane', comment: 'comment' }, { username: 'imer', comment: 'comment' }, { username: 'Cake', comment: 'comment' }, { username: 'Suzana', comment: 'comment' }, { username: 'Tinaye', comment: 'comment' }, { username: 'Andrea', comment: 'comment' }];
+  const length = await counter1(API_FETCH);
+  expect(length).toEqual(6);
+});
